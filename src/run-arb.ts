fb4ae18d674f094ae5b3ae5a8e144ba9c7152bdd
thereal-atom <oscarfal2006@gmail.com>
2025-02-03 16:38:08 +0000

    .

--- src/run-arb.ts
+++ src/run-arb.ts
@@ -224,6 +224,6 @@ const runArb = async () => {
 	}
 };
 
-await runArb();
+// await runArb();
 
-// setInterval(runArb, config.arbConfig.attemptInterval);
+setInterval(runArb, config.arbConfig.attemptInterval);
